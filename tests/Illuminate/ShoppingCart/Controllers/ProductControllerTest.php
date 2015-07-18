36c72c9bc59ebba374659fb58afb6cce4e392d8d
huytbt <huytbt@gmail.com>
2015-07-18 09:13:13 +0700

    Update test validation on ProductControllerTest

--- tests/Illuminate/ShoppingCart/Controllers/ProductControllerTest.php
+++ tests/Illuminate/ShoppingCart/Controllers/ProductControllerTest.php
@@ -8,8 +8,15 @@ class ProductControllerTest extends TestCase
 
     public function testCreateValidateFailure()
     {
-        $response = $this->call('POST', '/products', [ 'title' => 'asds' ]);
+        $res = $this->call('POST', '/products');
+        $this->assertEquals(400, $res->getStatusCode());
 
-        dump($response);
+        $results = json_decode($res->getContent());
+        $this->assertEquals('error', $results->status);
+        $this->assertEquals('validation', $results->type);
+        $this->assertEquals('The title field is required.', $results->first_message);
+        $this->assertObjectHasAttribute('title', $results->errors);
+        $this->assertInternalType('array', $results->errors->title);
+        $this->assertEquals('The title field is required.', $results->errors->title[0]);
     }
 }

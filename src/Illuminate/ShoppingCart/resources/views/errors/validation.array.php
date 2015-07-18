2dd6e652ce644769a5fa2322a46b10ad205edcfc
huytbt <huytbt@gmail.com>
2015-07-18 09:00:22 +0700

    Update error validation view

--- src/Illuminate/ShoppingCart/resources/views/errors/validation.array.php
+++ src/Illuminate/ShoppingCart/resources/views/errors/validation.array.php
@@ -1,3 +1,7 @@
 <?php
 
+$this->set('version', '1.0');
+$this->set('status', 'error');
+$this->set('type', 'validation');
 $this->set('errors', $errors);
+$this->set('first_message', $errors->first());

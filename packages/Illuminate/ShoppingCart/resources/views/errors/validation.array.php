72fe39763199bb3e0fc224a50fe925b45ddabdc9
huytbt <huytbt@gmail.com>
2015-07-26 13:17:17 +0700

    Update errors/validation.array.php

--- packages/Illuminate/ShoppingCart/resources/views/errors/validation.array.php
+++ packages/Illuminate/ShoppingCart/resources/views/errors/validation.array.php
@@ -4,4 +4,4 @@ $this->set('version', '1.0');
 $this->set('status', 'error');
 $this->set('type', 'validation');
 $this->set('errors', $errors);
-$this->set('first_message', $errors->first());
+$this->set('message', $errors->first());

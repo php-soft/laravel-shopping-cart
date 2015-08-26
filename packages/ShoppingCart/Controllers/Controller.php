d0070a0ef6da20a60ff90c4086c84726f0742a13
huytbt <huytbt@gmail.com>
2015-08-26 16:31:23 +0700

    Change namespace from PhpSoft\Illuminate\ArrayView to PhpSoft\ArrayView #22

--- packages/ShoppingCart/Controllers/Controller.php
+++ packages/ShoppingCart/Controllers/Controller.php
@@ -2,8 +2,6 @@
 
 namespace PhpSoft\ShoppingCart\Controllers;
 
-use Auth;
-use PhpSoft\Illuminate\ArrayView\Facades\ArrayView;
 use App\Http\Controllers\Controller as AppController;
 
 class Controller extends AppController

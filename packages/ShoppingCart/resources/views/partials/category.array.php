7d8c3c3b2654224e9e081980e623a249e173c65b
Huy Ta <huytbt@gmail.com>
2015-11-15 12:54:41 +0700

    Remove created_at in category template #41

--- packages/ShoppingCart/resources/views/partials/category.array.php
+++ packages/ShoppingCart/resources/views/partials/category.array.php
@@ -9,7 +9,6 @@ $this->extract($category, [
     'parent_id',
     'order',
     'status',
-    'created_at',
 ]);
 $this->set('parentId', $category->parent_id);
 $this->set('createdAt', date('c', strtotime($category->created_at)));
